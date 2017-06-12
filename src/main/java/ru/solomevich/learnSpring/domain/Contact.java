348c4859cee9d308b3b561cc8d7caf93e165fff2
Sergey Solomevich <sergeysolomevich@gmail.com>
2017-06-12 23:11:07 +0300

    commit 1 - create domain

---
+++ src/main/java/ru/solomevich/learnSpring/domain/Contact.java
@@ -0,0 +1,34 @@
+package ru.solomevich.learnSpring.domain;
+
+/**
+ * Created by 15 on 12.06.2017.
+ */
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "CONTACTS")
+public class Contact {
+
+    @Id
+    @Column(name = "ID")
+    @GeneratedValue
+    private Integer id;
+
+    @Column(name = "FIRSTNAME")
+    private String firstname;
+
+    @Column(name = "LASTNAME")
+    private String lastname;
+
+    @Column(name = "EMAIL")
+    private String email;
+
+    @Column(name = "TELEPHONE")
+    private String telephone;
+
+    // Getters and setters
+}
